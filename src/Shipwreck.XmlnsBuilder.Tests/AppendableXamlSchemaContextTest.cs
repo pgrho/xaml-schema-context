2dfcd057a130c028541f533b45147929209ddaea
pgrho <shipwreck@live.jp>
2021-04-19 13:53:22 +0900

    AppendableXamlSchemaContext

---
+++ src/Shipwreck.XmlnsBuilder.Tests/AppendableXamlSchemaContextTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Xaml;
+using System.Xml.Linq;
+using Xunit;
+
+namespace Shipwreck.XmlnsBuilder
+{
+    public class AppendableXamlSchemaContextTest
+    {
+        private static AppendableXamlSchemaContext GetContext()
+            => new AppendableXamlSchemaContext()
+                .AddNamespace("test://sys", typeof(int).Namespace, typeof(int).Assembly.GetName().Name);
+
+        [Theory]
+        [InlineData("<Int32 xmlns='clr-namespace:System;assembly=mscorlib'>0</Int32>", typeof(int), "0")]
+        [InlineData("<Byte xmlns='test://sys'>10</Byte>", typeof(byte), "10")]
+        public void DeserializationTest(string xaml, Type expectedType, string expectedValue)
+        {
+            var sc = GetContext();
+            using (var sr = new StringReader(xaml))
+            using (var xxr = new XamlXmlReader(sr, sc))
+            using (var xow = new XamlObjectWriter(sc))
+            {
+                while (xxr.Read())
+                {
+                    xow.WriteNode(xxr);
+                }
+
+                Assert.IsType(expectedType, xow.Result);
+                Assert.Equal(
+                    expectedValue,
+                    (xow.Result as IFormattable)?.ToString(null, CultureInfo.InvariantCulture) ?? xow.Result?.ToString());
+            }
+        }
+
+        [Theory]
+        [InlineData(typeof(AppendableXamlSchemaContextTest), "clr-namespace:Shipwreck.XmlnsBuilder;assembly=Shipwreck.XmlnsBuilder.Tests", nameof(AppendableXamlSchemaContextTest))]
+        [InlineData(typeof(int), "test://sys", nameof(Int32))]
+        public void SerializeTest(Type valueType, string xmlns, string name)
+        {
+            var v = Activator.CreateInstance(valueType);
+
+            var sc = GetContext();
+            using (var xor = new XamlObjectReader(v, sc))
+            using (var sw = new StringWriter())
+            using (var xxw = new XamlXmlWriter(sw, sc))
+            {
+                while (xor.Read())
+                {
+                    xxw.WriteNode(xor);
+                }
+
+                xxw.Flush();
+
+                var xaml = sw.ToString();
+
+                var xd = XDocument.Parse(xaml);
+
+                Assert.Equal(XName.Get(name, xmlns), xd.Root.Name);
+            }
+        }
+    }
+}
---
+++ src/Shipwreck.XmlnsBuilder/AppendableXamlSchemaContext.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xaml;
+using System.Xaml.Schema;
+
+namespace Shipwreck.XmlnsBuilder
+{
+    public class AppendableXamlSchemaContext : XamlSchemaContext
+    {
+        protected sealed class CustomXamlType : XamlType
+        {
+            public CustomXamlType(Type underlyingType, AppendableXamlSchemaContext schemaContext)
+                : base(underlyingType, schemaContext)
+            {
+            }
+
+            public CustomXamlType(string typeName, IList<XamlType> typeArguments, AppendableXamlSchemaContext schemaContext)
+                : base(typeName, typeArguments, schemaContext)
+            {
+            }
+
+            public CustomXamlType(Type underlyingType, AppendableXamlSchemaContext schemaContext, XamlTypeInvoker invoker)
+                : base(underlyingType, schemaContext, invoker)
+            {
+            }
+
+            public CustomXamlType(string unknownTypeNamespace, string unknownTypeName, IList<XamlType> typeArguments, AppendableXamlSchemaContext schemaContext)
+                : base(unknownTypeNamespace, unknownTypeName, typeArguments, schemaContext)
+            {
+            }
+
+            private string[] _CustomNamespaces;
+
+            public override IList<string> GetXamlNamespaces()
+            {
+                if (_CustomNamespaces == null)
+                {
+                    var nss = ((AppendableXamlSchemaContext)SchemaContext)._Namespaces;
+                    var asm = UnderlyingType.Assembly.GetName().Name;
+                    var ns = UnderlyingType.Namespace;
+                    _CustomNamespaces = nss.Where(e => e.Value.asm == asm && e.Value.ns == ns).Select(e => e.Key).ToArray();
+                }
+
+                if (_CustomNamespaces.Any())
+                {
+                    return _CustomNamespaces.Concat(base.GetXamlNamespaces()).ToList();
+                }
+
+                return base.GetXamlNamespaces();
+            }
+        }
+
+        private Dictionary<string, (string asm, string ns)> _Namespaces;
+        private Dictionary<string, string> _Prefixes;
+
+        public AppendableXamlSchemaContext AddNamespaces(string xmlns, IEnumerable<Type> namespaceTypes)
+        {
+            foreach (var t in namespaceTypes)
+            {
+                AddNamespace(xmlns, t);
+            }
+            return this;
+        }
+
+        public AppendableXamlSchemaContext AddNamespace(string xmlns, Type namespaceType)
+            => AddNamespace(xmlns, namespaceType.Namespace, namespaceType.Assembly.GetName().Name);
+
+        public AppendableXamlSchemaContext AddNamespace(string xmlns, string clrNamespace, string assemblyName)
+        {
+            (_Namespaces ??= new Dictionary<string, (string asm, string ns)>())[xmlns] = (assemblyName, clrNamespace);
+            return this;
+        }
+
+        public AppendableXamlSchemaContext AddPrefix(string xmlns, string prefix)
+        {
+            (_Prefixes ??= new Dictionary<string, string>())[xmlns] = prefix;
+            return this;
+        }
+
+        public override IEnumerable<string> GetAllXamlNamespaces()
+            => _Namespaces?.Count > 0 ? base.GetAllXamlNamespaces().Concat(_Namespaces.Keys).Distinct() : base.GetAllXamlNamespaces();
+
+        public override ICollection<XamlType> GetAllXamlTypes(string xamlNamespace)
+            => base.GetAllXamlTypes(xamlNamespace);
+
+        public override string GetPreferredPrefix(string xmlns)
+        {
+            if (_Prefixes != null && _Prefixes.TryGetValue(xmlns, out var r))
+            {
+                return r;
+            }
+            return base.GetPreferredPrefix(xmlns);
+        }
+
+        protected override XamlType GetXamlType(string xamlNamespace, string name, params XamlType[] typeArguments)
+        {
+            if (_Namespaces != null && _Namespaces.TryGetValue(xamlNamespace, out var tp))
+            {
+                var xt = base.GetXamlType($"clr-namespace:{tp.ns};assembly={tp.asm}", name, typeArguments)
+                        ?? base.GetXamlType($"clr-namespace:{tp.ns}", name, typeArguments);
+
+                if (xt != null)
+                {
+                    return xt;
+                }
+            }
+            return base.GetXamlType(xamlNamespace, name, typeArguments);
+        }
+
+        private readonly ConcurrentDictionary<Type, CustomXamlType> _Types = new ConcurrentDictionary<Type, CustomXamlType>();
+
+        public override XamlType GetXamlType(Type type)
+        {
+            if (!_Types.TryGetValue(type, out var r))
+            {
+                r = new CustomXamlType(type, this);
+                _Types.TryAdd(type, r);
+            }
+            return r;
+        }
+
+        public override bool TryGetCompatibleXamlNamespace(string xamlNamespace, out string compatibleNamespace)
+        {
+            if (_Namespaces != null && _Namespaces.ContainsKey(xamlNamespace))
+            {
+                compatibleNamespace = xamlNamespace;
+                return true;
+            }
+            return base.TryGetCompatibleXamlNamespace(xamlNamespace, out compatibleNamespace);
+        }
+    }
+}
